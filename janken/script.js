4f939567f81cf2d87207844de53898244785328e
ttexan1 <ttexan.developper@gmail.com>
2019-06-05 19:14:47 +0900

    refactor script

--- janken/script.js
+++ janken/script.js
@@ -1,34 +1,43 @@
+prepareAudio = new Audio('audio/prepare.mp3')
+loseSound = new Audio('audio/lose_sound.mp3')
+winSound = new Audio('audio/win_sound.mp3')
+
 for (var i=0; i <3; i++) {
   document.getElementsByClassName("hand")[i].addEventListener("click", function(e){
     document.body.classList = []
     userHand = e.target.getAttribute('data-hand');
+    displayUserHand(userHand)
     machineHand = Math.floor( Math.random() * (3))
-    var handMap = ["rock","scissors","paper"];
-    result = (userHand - machineHand) % 3
-    new Audio('audio/prepare.mp3').play();
+    result = (userHand - machineHand + 3) % 3
+    prepareAudio.play();
     var waitTime = 2000
-    setTimeout(displayHands, waitTime, userHand, machineHand)
-    setTimeout(judgeResult, waitTime+100, result)
+    setTimeout(displayMachineHand, waitTime, machineHand)
   }, false);
 }
+prepareAudio.addEventListener('ended',
+  judgeResult, false);
 
-function judgeResult(result){
+function judgeResult(){
   if (result == 0){
     alert("あいこ！もう一回勝負！")
   } else if (result == 1 ) {
-    new Audio('audio/lose_sound.mp3').play();
+    loseSound.play()
     document.body.classList.add("lose")
   } else {
-    new Audio('audio/win_sound.mp3').play();
+    winSound.play()
     document.body.classList.add("win")
   }
 }
 
-function displayHands(u_hand, m_hand){
+function displayUserHand(u_hand){
   var user_hand_area = document.getElementById("user_hand_area")
-  var machine_hand_area = document.getElementById("machine_hand_area")
   var u_hands = ["✊", "✌️", "✋"]
-  var m_hands = ["images/rock.jpg", "images/scissors.jpg", "images/paper.jpg"]
   user_hand_area.textContent = u_hands[u_hand]
-  machine_hand_area.children[0].src = m_hands[m_hand]
+}
+
+function displayMachineHand(m_hand){
+  var machine_hand_area = document.getElementById("machine_hand_area")
+  var imagePrefix = "images/"
+  var m_hands = ["rock.jpg", "scissors.jpg", "paper.jpg"]
+  machine_hand_area.children[0].src = imagePrefix + m_hands[m_hand]
 }
