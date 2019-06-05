b39da34a9dc1de815e3eb40d2160307054bd676e
ttexan1 <ttexan.developper@gmail.com>
2019-06-05 17:37:46 +0900

    fix stupid hand

--- janken/script.js
+++ janken/script.js
@@ -3,10 +3,7 @@ for (var i=0; i <3; i++) {
     document.body.classList = []
     userHand = e.target.getAttribute('data-hand');
     machineHand = Math.floor( Math.random() * (3))
-    machineHand = 0
     var handMap = ["rock","scissors","paper"];
-    console.log(handMap[userHand]);
-    console.log(handMap[machineHand]);
     result = (userHand - machineHand) % 3
     new Audio('audio/prepare.mp3').play();
     var waitTime = 2000
@@ -17,24 +14,21 @@ for (var i=0; i <3; i++) {
 
 function judgeResult(result){
   if (result == 0){
-    console.log("aiko");
     alert("あいこ！もう一回勝負！")
   } else if (result == 1 ) {
-    console.log("you lose");
     new Audio('audio/lose_sound.mp3').play();
     document.body.classList.add("lose")
   } else {
-    console.log("you win");
     new Audio('audio/win_sound.mp3').play();
     document.body.classList.add("win")
   }
 }
 
 function displayHands(u_hand, m_hand){
-  user_hand_area = document.getElementById("user_hand_area")
-  machine_hand_area = document.getElementById("machine_hand_area")
-  u_hands = ["✊", "✌️", "✋"]
-  m_hands = ["images/rock.jpg", "images/scissors.jpg", "images/paper.jpg"]
+  var user_hand_area = document.getElementById("user_hand_area")
+  var machine_hand_area = document.getElementById("machine_hand_area")
+  var u_hands = ["✊", "✌️", "✋"]
+  var m_hands = ["images/rock.jpg", "images/scissors.jpg", "images/paper.jpg"]
   user_hand_area.textContent = u_hands[u_hand]
   machine_hand_area.children[0].src = m_hands[m_hand]
 }
